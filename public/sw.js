6c69beaacfb3c57c40e10c89767952c5db465c86
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-25 01:36:06 +0300

    chore(v0.2): add public/sw.js

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => caches.delete(key)));
+    await self.registration.unregister();
+  })());
+});
